7f94ed7a38e738d681594fda8e5081e6f3c0f248
mezaouifinance <mehdizaoui0608@gmail.com>
2026-07-03 11:22:00 +0200

    fix theta missing from exports

--- src/optkit/__init__.py
+++ src/optkit/__init__.py
@@ -1,10 +1,10 @@
-from .bs import bs_price, bs_delta, bs_gamma, bs_vega
+from .bs import bs_price, bs_delta, bs_gamma, bs_vega, bs_theta, bs_rho
 from .binomial import crr_price
 from .implied_vol import implied_vol
 from .noarb import bounds_vanilla, put_call_parity_residual
 
 __all__ = [
-    "bs_price", "bs_delta", "bs_gamma", "bs_vega", "bs_theta",
+    "bs_price", "bs_delta", "bs_gamma", "bs_vega", "bs_theta", "bs_rho",
     "crr_price",
     "implied_vol",
     "bounds_vanilla", "put_call_parity_residual",
